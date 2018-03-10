e84c85785763a06c6cd67fd56b648d282c66f3df
Shashikant Sukenkar <sukenkar@hotmail.com>
2018-03-10 06:43:38 +0530

    Compass & map
    Compass & map

--- app/src/main/java/com/avenueinfotech/compasslocation/MainActivity.java
+++ app/src/main/java/com/avenueinfotech/compasslocation/MainActivity.java
@@ -1,20 +1,46 @@
 package com.avenueinfotech.compasslocation;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     ImageView img_compass;
-//    TextView txt_azimuth;
+    //    TextView txt_azimuth;
     int mAzimuth;
     private SensorManager mSensorManager;
     private Sensor mRotationV, mAccelerometer, mMagnetometer;
@@ -26,6 +52,27 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private boolean mLastAccelerometerSet = false;
     private boolean mLastMagnetometerSet = false;
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+
+    protected Location mLastLocation;
+
+    private String mLatitudeLabel;
+    private String mLongitudeLabel;
+    private String mAltitudelabel;
+    private TextView mLatitudeText;
+    private TextView mLongitudeText;
+    private TextView mAltitudeText;
+
+    private Button location;
+
+
 
 
     @Override
@@ -33,35 +80,60 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        img_compass = (ImageView)findViewById(R.id.img_compass);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        img_compass = (ImageView) findViewById(R.id.img_compass);
 //        txt_azimuth = (TextView)findViewById(R.id.txt_azimuth);
 
+        mLatitudeLabel = getResources().getString(R.string.latitude_label);
+        mLongitudeLabel = getResources().getString(R.string.longitude_label);
+        mAltitudelabel = getResources().getString(R.string.altitude_label);
+        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
+        mLongitudeText = (TextView) findViewById((R.id.longitude_text));
+        mAltitudeText = (TextView)findViewById(R.id.altitude_text);
+
+        location = (Button)findViewById(R.id.location);
+
+
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+
         start();
+
+        itlocation();
+    }
+
+    private void itlocation() {
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent locationTracker = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(locationTracker);
+             }
+        });
     }
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if(event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
-            SensorManager.getRotationMatrixFromVector(rMat,event.values);
-            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat,orientation)[0]+360)%360);
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0] + 360) % 360);
 
         }
-        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            System.arraycopy(event.values,0,mLastAccelerometer,0,event.values.length);
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
             mLastAccelerometerSet = true;
-        }
-        else
-        if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
-            System.arraycopy(event.values,0,mLastMagnetometer,0,event.values.length);
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
             mLastMagnetometerSet = true;
         }
 
-        if(mLastMagnetometerSet && mLastAccelerometerSet){
-            SensorManager.getRotationMatrix(rMat,null, mLastAccelerometer,mLastMagnetometer);
-            SensorManager.getOrientation(rMat,orientation);
-            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat,orientation)[0]+360)%360);
+        if (mLastMagnetometerSet && mLastAccelerometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0] + 360) % 360);
 
         }
 
@@ -90,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 //        txt_azimuth.setText(mAzimuth+"° "+where );
 
 
-        }
+    }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -98,29 +170,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private void start() {
-        if(mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)==null){
-            if(mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)==null ||
-                    mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)==null){
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
+            if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null ||
+                    mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) {
                 noSensorAlert();
-            }
-            else {
+            } else {
                 mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
                 mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-                haveSensor = mSensorManager.registerListener(this,mAccelerometer,SensorManager.SENSOR_DELAY_UI);
-                haveSensor2 = mSensorManager.registerListener(this,mMagnetometer,SensorManager.SENSOR_DELAY_UI);
+                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+                haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
 
 
             }
-        }
-        else {
+        } else {
             mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-            haveSensor = mSensorManager.registerListener(this,mRotationV,SensorManager.SENSOR_DELAY_UI);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
 
+        }
+
+        if (!checkPermissions()) {
+            requestPermissions();
+        } else {
+            getLastLocation();
         }
     }
 
-    public void noSensorAlert(){
+    public void noSensorAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
         alertDialog.setMessage("Your Device doesn't support Compass")
                 .setCancelable(false)
@@ -132,15 +208,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 });
     }
 
-    public void stop(){
-        if(haveSensor && haveSensor2){
-            mSensorManager.unregisterListener(this,mAccelerometer);
-            mSensorManager.unregisterListener(this,mMagnetometer);
+    public void stop() {
+        if (haveSensor && haveSensor2) {
+            mSensorManager.unregisterListener(this, mAccelerometer);
+            mSensorManager.unregisterListener(this, mMagnetometer);
+        } else if (haveSensor) {
+            mSensorManager.unregisterListener(this, mRotationV);
         }
-        else
-            if(haveSensor){
-            mSensorManager.unregisterListener(this,mRotationV);
-            }
     }
 
     @Override
@@ -154,6 +228,74 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
         start();
     }
+
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            mLastLocation = task.getResult();
+
+                            mLatitudeText.setText(String.format(Locale.ENGLISH, "%s: %f",
+                                    mLatitudeLabel,
+                                    mLastLocation.getLatitude()));
+                            mLongitudeText.setText(String.format(Locale.ENGLISH, "%s: %f",
+                                    mLongitudeLabel,
+                                    mLastLocation.getLongitude()));
+                            mAltitudeText.setText(String.format(Locale.ENGLISH, "%s: %f",
+                                    mAltitudelabel,
+                                    mLastLocation.getAltitude()));
+                        } else {
+                            Log.w(TAG, "getLastLocation:exception", task.getException());
+//                            showSnackbar(getString(R.string.no_location_detected));
+                        }
+                    }
+                });
+    }
+
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_PERMISSIONS_REQUEST_CODE);
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+
+//            showSnackbar(R.string.permission_rationale, android.R.string.ok,
+//                    new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View view) {
+//                            // Request permission
+//                            startLocationPermissionRequest();
+//                        }
+//                    });
+
+        } else {
+            Log.i(TAG, "Requesting permission");
+
+            startLocationPermissionRequest();
+        }
+    }
+
 
 
 }
